e671cf76cbcc234167b368c4e7b45b8f715c97b2
Alexey Spiridonov <ZayacZloy@yandex.ru>
2022-11-02 18:22:43 +0300

    feat: confirm on ServiceCard button

--- src/components/ServiceCard/ServiceCard.tsx
+++ src/components/ServiceCard/ServiceCard.tsx
@@ -1,4 +1,4 @@
-import React, { FC, ReactNode, ChangeEvent } from 'react';
+import React, { FC, ReactNode, ChangeEvent, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { ServiceType } from '../ServicesForm/ServicesForm';
 
@@ -8,15 +8,22 @@ type PropsType = {
 }
 
 const ServiceCard: FC<PropsType> = ({ service, handleDeleteService }) => {
+
+  const [buttonCounter, setButtonCounter] = useState<number>(0);
   
   const handleButtonClick = () => {
-    handleDeleteService(service);
+    if (buttonCounter === 0) {
+      setButtonCounter(buttonCounter + 1);
+    } else if(buttonCounter === 1) {
+      setButtonCounter(0);
+      handleDeleteService(service);
+    } 
   }
 
   return (
     <li key={`${service.price}${service.name}`} className="service">
       <h2 className="service__title">{service.name}: <span className='service__span'>{service.price} руб.</span></h2>
-      <button onClick={handleButtonClick} type="button" className="service__button" aria-label="Удалить услугу">Удалить</button>
+      <button onClick={handleButtonClick} type="button" className={`${buttonCounter === 1 ?'service__button_type_warning' : ''} service__button`} aria-label="Удалить услугу">Удалить</button>
     </li>
   )
 }
