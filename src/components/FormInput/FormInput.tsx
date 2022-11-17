d07799b2a8bdf932b6ab688f252cd942327cb916
Alexey Spiridonov <ZayacZloy@yandex.ru>
2022-11-17 19:55:32 +0300

    fix: fixed form input, added step to number inputs

--- src/components/FormInput/FormInput.tsx
+++ src/components/FormInput/FormInput.tsx
@@ -15,7 +15,9 @@ const FormInput: FC<PropsType> = ({ onChange, value, type, error, name, label, r
   return (
     <label className='form-input__label'>
       { isLabelVisible ? `${label}` : null}
-      <input placeholder={label} onChange={onChange} value={value || ''} type={type} className="form-input" name={name} id={name} required={required} />
+      {type === 'number' ? 
+      <input step=".1" placeholder={label} onChange={onChange} value={value || ''} type={type} className="form-input" name={name} id={name} required={required} />
+      : <input placeholder={label} onChange={onChange} value={value || ''} type={type} className="form-input" name={name} id={name} required={required} />}
       <span className={`form-input__error`}>{error}</span>
     </label>
   )
