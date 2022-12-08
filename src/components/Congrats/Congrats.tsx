fda3a4070688c7fce006babf566246206efc69a3
Alexey Spiridonov <ZayacZloy@yandex.ru>
2022-12-08 17:17:27 +0300

    feat: added Congrats

--- src/components/Congrats/Congrats.tsx
+++ src/components/Congrats/Congrats.tsx
@@ -35,16 +35,16 @@ const Congrats: FC<PropsType> = ({ isCongratsOpen, handleCloseCongrats }) => {
           <h2 className={`congrats__subtitle`}>
             Поздравляю тебя с Днем Рождения!
           </h2>
-          <p className="congrats__text">
+          <p className="congrats__text congrats__text_type_first">
             Хочу пожелать тебе - всегда оставаться такой яркой, красивой, позитивной!
           </p>
-          <p className="congrats__text">
+          <p className="congrats__text congrats__text_type_second">
             Ты - невероятно добрый человек. Оставайся такой и пусть тебе встречаются только такие же добрые люди!
           </p>
-          <p className="congrats__text">
-            И я знаю - ты очень любишь свою работу. Пусть от количества заказов перегревается твой телефон! А чтобы ты не тратила свое драгоценное время на расчеты - существует это приложение.
+          <p className="congrats__text congrats__text_type_third">
+            И я знаю - ты очень любишь свою работу. Пусть от количества заказов перегревается твой телефон! А чтобы ты не тратила свое драгоценное время на расчеты - существует это приложение. Оно будет становиться только лучше, в том числе - благодаря тебе.
           </p>
-          <h3 className="congrats__subtitle1">
+          <h3 className="congrats__congratulations">
             И еще раз - с Днем Рождения!
           </h3>
           <p className="congrats__text congrats__text_type_ps">
